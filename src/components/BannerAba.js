a46ecdf42be3d4f0919d68ef0397ebeb5190f2db
shawnpmarcotte <shawnmarcottejr@gmail.com>
2020-04-24 11:53:55 -0400

    enhancement-50 ABA/Core Symptoms content updated

--- src/components/BannerAba.js
+++ src/components/BannerAba.js
@@ -5,6 +5,10 @@ const BannerAba = props => (
     <div className="inner">
       <header className="major">
         <h1>The Core Symtoms of Autism</h1>
+        <h3> • Difficulty communicating with others</h3>
+        <h3> • Repetitive and restricted behavior and actions</h3>
+        <h3> • Persistant interferance with regular living</h3>
+        <h3> • Can start early on in childhood (and may go unnoticed and unrecognized)</h3>
       </header>
       <div className="content"></div>
     </div>
