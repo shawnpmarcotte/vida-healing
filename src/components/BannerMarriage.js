8e874aeb199b5497f0701c35999440f2d32a8d2f
shawnpmarcotte <shawnmarcottejr@gmail.com>
2020-04-24 12:26:11 -0400

    enhancement-52 updated marriage/causes page banner

--- src/components/BannerMarriage.js
+++ src/components/BannerMarriage.js
@@ -4,7 +4,8 @@ const BannerMarriage = props => (
   <section id="banner" className="style2">
     <div className="inner">
       <header className="major">
-        <h1>Marriage And Couples Therapy</h1>
+        <h1>Autism: What are the Causes?</h1>
+        <h2>The Prevalence of Autism</h2>
       </header>
       <div className="content"></div>
     </div>
