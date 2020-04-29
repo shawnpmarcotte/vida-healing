b48a7c417ec9bf81772e26a70b2c55b6bfff8b9c
shawnpmarcotte <shawnmarcottejr@gmail.com>
2020-04-29 11:09:08 -0400

    enhancement-64 added join team/about/mission to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const Footer = props => (
   <footer id="footer">
     <div className="inner">
-      <ul className="copyright">
-        <li>&copy; Copyright © 2018 Vida Healing, LLC</li>
+      <ul className="icons">
+        <h4>
+          <a
+            href="/contact"
+            target="blank"
+          >
+            <span className="label">JOIN OUR TEAM</span>
+          </a>
+        </h4>
+        <h4>
+          <a
+            href="/mission"
+            target="blank"
+          >
+            <span className="label">MISSION</span>
+          </a>
+        </h4>
+        <h4>
+          <a
+            href="/about"
+            target="blank"
+          >
+            <span className="label">ABOUT</span>
+          </a>
+        </h4>
       </ul>
     </div>
   </footer>
