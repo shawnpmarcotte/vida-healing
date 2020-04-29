b7f13589242440a19738e7d987ea130da6dc053d
shawnpmarcotte <shawnmarcottejr@gmail.com>
2020-04-29 11:50:51 -0400

    enhancement-64 updated join our team page

--- src/pages/jointeam.js
+++ src/pages/jointeam.js
@@ -23,19 +23,37 @@ const JoinTeam = props => (
                         <section id="one">
                             <div className="inner">
                                 <header className="major">
-                                    <h4>
-                                        Family Therapy is a type of counseling that can help family members improve communication and resolve conflicts. At times, family <br />
-                    therapy is necessary to address family struggles and heal a family’s wounds. At Vida Healing, we strive to offer families a way to do this<br />
-                    in a safe, comfortable, and professional environment. We will assist in developing a plan for maintaining a healthy, functional family.
-                  </h4>
                                 </header>
+                                <h3>
+                                    As a Vida Healing employee, you will play a vital role in shaping the lives and future of our clients. For this reason, we are dedicated to creating an inspiring culture and a working environment that attracts the most passionate people in the ABA field.  You will be considered for both full-time and part-time employment. Our ideal candidates will demonstrate a commitment to learning and excellence in the profession of Applied Behavior analysis. We welcome applicants with the relevant credentials in any area relevant to ABA
+                                    </h3>
                                 <span className="image main"><img src={familysecondary} alt="" /></span>
                                 <h3>
-                                    Whether you are new parents, recently separated or divorced, and/or trying to manage the curve balls that come with raising a teenager, we are here to assist. Our goal is to establish strong communication skills and create a stronger and healthier family.
-                </h3>
+                                    Vida Healing currently has openings for potential employees with the appropriate credentials and training:
+                                </h3>
+                                <ul>
+                                    <li> •  BCBA - Board Certified Behavior Analyst</li>
+                                    <li> •  LMHC -Licensed Mental Health Counselors</li>
+                                    <li> •  RBT - Registered Behavior Technicians</li>
+                                </ul>
+                                <h3>
+                                    We are dedicated to living our adopted values as a driving force:
+                                </h3>
+                                <ul>
+                                    <li> •  Client Care</li>
+                                    <li> •  Integrity</li>
+                                    <li> •  Accountability</li>
+                                    <li> •  Education and Training</li>
+                                    <li> •  Administrative support</li>
+                                </ul>
+                            </div>
+                            <div className="inner">
                                 <h4>
-                                    If you are dealing with any of the issues above, please contact us today for an appointment
-                </h4>
+                                    PREFERRED QUALIFICATION: It is incumbent on all candidates applying to work with Vida Healing to have all the necessary qualifications for their position. Whether you’re interested in being an RBT, Behavior Analyst or Licensed Mental Health Counselor, we have information about what it’s like to work in our company and how to proceed
+                                </h4>
+                                <h4>
+                                    PREREQUISITE FOR HIRE: Background screening, First aid & CPR, HIPPA, Intro to Developmental Disabilities and Zero Tolerance
+                                </h4>
                             </div>
                         </section>
                     </div>
