ba97456b79fd2a6262bb58faede2990bd0be6431
shawnpmarcotte <shawnmarcottejr@gmail.com>
2020-04-28 20:21:57 -0400

    enhancement-62 updated family page content

--- src/components/BannerFamily.js
+++ src/components/BannerFamily.js
@@ -4,9 +4,7 @@ const BannerFamily = props => (
   <section id="banner" className="style2">
     <div className="inner">
       <header className="major">
-        <h1>Asperger's Syndrome: What is It?</h1>
-        <h3>Asperger syndrome is one of the most common syndromes on the autism spectrum. It became part of the umbrella of ASD (Autism Spectrum Disorder) in 2013, as well as the DSM-V five (Statistical and Diagnostic Manual of Mental Disorders)</h3>
-        <h3>Typical to strong verbal language abilities and intellectual ability distinguish Asperger Syndrome from other kinds of autism</h3>
+        <h1>Family Therapy</h1>
       </header>
       <div className="content"></div>
     </div>
---
+++ src/components/BannersAspergers.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BannerFamily = props => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Asperger's Syndrome: What is It?</h1>
+                <h3>Asperger syndrome is one of the most common syndromes on the autism spectrum. It became part of the umbrella of ASD (Autism Spectrum Disorder) in 2013, as well as the DSM-V five (Statistical and Diagnostic Manual of Mental Disorders)</h3>
+                <h3>Typical to strong verbal language abilities and intellectual ability distinguish Asperger Syndrome from other kinds of autism</h3>
+            </header>
+            <div className="content"></div>
+        </div>
+    </section>
+)
+
+export default BannerFamily
