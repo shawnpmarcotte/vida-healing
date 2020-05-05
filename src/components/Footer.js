5cec16bda48e2b2867f97dee34309326066b44af
shawnpmarcotte <shawnmarcottejr@gmail.com>
2020-05-05 13:25:45 -0400

    enhancement-107 footer and contact form updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,27 +5,27 @@ const Footer = props => (
   <footer id="footer">
     <div className="inner">
       <ul className="icons">
-        <h4>
+        <h3>
           <a
             href="/jointeam"
           >
             <span className="label">JOIN OUR TEAM</span>
           </a>
-        </h4>
-        <h4>
+        </h3>
+        <h3>
           <a
             href="/mission"
           >
             <span className="label">MISSION</span>
           </a>
-        </h4>
-        <h4>
+        </h3>
+        <h3>
           <a
             href="/about"
           >
             <span className="label">ABOUT</span>
           </a>
-        </h4>
+        </h3>
       </ul>
       <ul className="copyright">
         <li>&copy; Copyright © 2018 Vida Healing, LLC</li>
