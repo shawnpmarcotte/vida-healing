823254786df04023720cb7a999164e0f7da3b9a7
shawnpmarcotte <shawnmarcottejr@gmail.com>
2020-05-06 17:34:37 -0400

    enhancement-111 removed adults from autism facts

--- src/components/BannerJoinTeam.js
+++ src/components/BannerJoinTeam.js
@@ -6,7 +6,10 @@ const BannerJoinTeam = props => (
             <header className="major">
                 <h1>JOIN OUR TEAM</h1>
                 <h3>
-                    As a Vida Healing employee, you will play a vital role in shaping the lives and future of our clients. For this reason, we are dedicated to creating an inspiring culture and a working environment that attracts the most passionate people in the ABA field.  You will be considered for both full-time and part-time employment. Our ideal candidates will demonstrate a commitment to learning and excellence in the profession of Applied Behavior analysis. We welcome applicants with the relevant credentials in any area relevant to ABA
+                    As a Vida Healing employee, you will play a vital role in shaping the lives and future of our clients.
+                    For this reason, we are dedicated to creating an inspiring culture and a working environment that attracts
+                    the most passionate people in the ABA field.  Applicants will be considered for both full-time and part-time employment.
+                    Our ideal candidates will demonstrate a commitment to learning and excellence in the profession of Applied Behavior analysis. We welcome applicants with the relevant credentials in any area relevant to ABA
                 </h3>
             </header>
             <div className="content"></div>
