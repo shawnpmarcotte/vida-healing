25cc0b57ef270b9b6fbb3a27d11ef2ed50b4e682
shawnpmarcotte <shawnmarcottejr@gmail.com>
2020-05-18 13:40:16 -0400

    enhancement 133 page color scheme update

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => (
     <header id="header" className="alt">
         <Link to="/" className="logo"><strong>Vida Healing</strong> </Link>
         <nav>
-            <a className="autism-link" onClick={props.onToggleAutismMenu} href="AutismResources:;">AUTISM RESOURCES</a>
+            <a onClick={props.onToggleAutismMenu} href="AutismResources:;">AUTISM RESOURCES</a>
         </nav>
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="Menu:;">MENU</a>
