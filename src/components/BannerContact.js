db07a9debcb8539c971eb980912d0f6d16b4ed05
shawnpmarcotte <shawnmarcottejr@gmail.com>
2020-06-01 18:55:08 -0400

    bug 152 added banner to contact page

---
+++ src/components/BannerContact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BannerContact = props => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Get in touch and find out how we can help!</h1>
+            </header>
+        </div>
+    </section>
+)
+
+export default BannerContact
