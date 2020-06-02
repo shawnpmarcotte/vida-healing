5b0193609d7853e474c8a8224a2db80667fba708
josedani08200 <jose.dani08200@gmail.com>
2020-06-01 20:17:16 -0400

    AR and Menu on hover hand glove thing

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
-const Header = (props) => (
-    <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>Vida Healing</strong> </Link>
-        <nav>
-            <a onClick={props.onToggleAutismMenu}>AUTISM RESOURCES</a>
-        </nav>
-        <nav>
-            <a className="menu-link" onClick={props.onToggleMenu}>MENU</a>
-        </nav>
-    </header>
+const Header = props => (
+  <header id="header" className="alt">
+    <Link to="/" className="logo">
+      <strong>Vida Healing</strong>{' '}
+    </Link>
+    <nav>
+      <a onClick={props.onToggleAutismMenu}>AUTISM RESOURCES</a>
+    </nav>
+    <nav>
+      <a className="menu-link" onClick={props.onToggleMenu}>
+        MENU
+      </a>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
-    onToggleAutismMenu: PropTypes.func,
-    onToggleMenu: PropTypes.func,
+  onToggleAutismMenu: PropTypes.func,
+  onToggleMenu: PropTypes.func,
 }
 
 export default Header
