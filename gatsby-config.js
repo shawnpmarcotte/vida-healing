039b835f5e1aa991bf2e61b056aa1759d09acbda
shawnpmarcotte <shawnmarcottejr@gmail.com>
2020-06-03 18:32:50 -0400

    form edit 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,41 +4,41 @@ module.exports = {
     author: "Nelida Henderson",
     description: "Vida Healing | Applied Behavior Analysis Therapy"
   },
-  plugins: [
+  plugins:
+    [
+      `gatsby-plugin-react-helmet`,
+      `gatsby-plugin-sass`,
+      {
+        resolve: `gatsby-plugin-styled-components`,
+        // resolve: `gatsby-plugin-manifest`,
+        // options: {
+        //   name: "GatsbyJS",
+        //   short_name: "GatsbyJS",
+        //   start_url: "/",
+        //   background_color: "#6b37bf",
+        //   theme_color: "#6b37bf",
+        //   // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        //   // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        //   display: "standalone",
+        //   icon: "src/assets/images/main-banner.jpg", // This path is relative to the root of the site.
+        //   // An optional attribute which provides support for CORS check.
+        //   // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        //   // Any invalid keyword or empty string defaults to `anonymous`
+        //   crossOrigin: `use-credentials`,
+        // },
 
-    {
-      resolve: `gatsby-plugin-styled-components`,
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "GatsbyJS",
-        short_name: "GatsbyJS",
-        start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "src/assets/images/main-banner.jpg", // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`,
       },
-
-    },
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/main-banner.jpg', // This path is relative to the root of the site.
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: 'gatsby-starter-default',
+          short_name: 'starter',
+          start_url: '/',
+          background_color: '#663399',
+          theme_color: '#663399',
+          display: 'minimal-ui',
+          icon: 'src/assets/images/main-banner.jpg', // This path is relative to the root of the site.
+        },
       },
-    },
-    'gatsby-plugin-sass',
-  ],
+    ],
 }
